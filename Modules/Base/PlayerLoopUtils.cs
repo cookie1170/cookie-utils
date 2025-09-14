74b2a0ca9b5d300b7a58feeeb71d8c323c07f737
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-09-14 18:16:13 +1000

    rewrite the timer class to hook into the player loop

---
+++ Modules/Base/PlayerLoopUtils.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+namespace CookieUtils
+{
+    public static class PlayerLoopUtils
+    {
+        public static void RemoveSystem(ref PlayerLoopSystem loop, in PlayerLoopSystem system)
+        {
+            if (loop.subSystemList == null) return;
+            
+            var systems = new List<PlayerLoopSystem>(loop.subSystemList);
+            for (int i = 0; i < systems.Count; i++) {
+                var subSystem = systems[i];
+                if (subSystem.type == system.type && subSystem.updateDelegate == system.updateDelegate) {
+                    systems.RemoveAt(i);
+                    loop.subSystemList = systems.ToArray();
+                }
+            }
+
+            HandleSubsystemRemove(ref loop, in system);
+        }
+
+        private static void HandleSubsystemRemove(ref PlayerLoopSystem loop, in PlayerLoopSystem system)
+        {
+            for (int i = 0; i < loop.subSystemList.Length; i++) {
+                var subSystem = loop.subSystemList[i];
+                RemoveSystem(ref subSystem, in system);
+            }
+        }
+
+        public static bool InsertSystem<T>(ref PlayerLoopSystem loop, in PlayerLoopSystem system, int index)
+        {
+            if (loop.type != typeof(T)) {
+                if (loop.type != null) Debug.Log($"Loop type {loop.type.Name} not equal to {typeof(T).Name}");
+                return HandleSubsystemInsert<T>(ref loop, in system, index);
+            }
+            
+            var systemList = new List<PlayerLoopSystem>();
+
+            if (loop.subSystemList != null) systemList.AddRange(loop.subSystemList);
+            
+            systemList.Insert(index, system);
+            loop.subSystemList = systemList.ToArray();
+            return true;
+        }
+
+        private static bool HandleSubsystemInsert<T>(ref PlayerLoopSystem loop, in PlayerLoopSystem system, int index)
+        {
+            if (loop.subSystemList == null) {
+                return false;
+            }
+
+            for (int i = 0; i < loop.subSystemList.Length; i++) {
+                if (!InsertSystem<T>(ref loop.subSystemList[i], in system, index)) {
+                    continue;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void PrintLoop(PlayerLoopSystem loop)
+        {
+            StringBuilder builder = new();
+            builder.AppendLine("Player loop:");
+
+            foreach (var subSystem in loop.subSystemList) {
+                builder.AppendLine(subSystem.type.Name);
+                HandleSubsystemPrint(loop, ref builder, 1);
+            }
+
+            Debug.Log(builder.ToString());
+        }
+
+        private static void HandleSubsystemPrint(PlayerLoopSystem loop, ref StringBuilder builder, int depth)
+        {
+            if (loop.subSystemList == null) return;
+
+            foreach (var subSystem in loop.subSystemList) {
+                for (int i = 0; i < depth * 2; i++)
+                    builder.Append(' ');
+                
+                builder.AppendLine( $"{subSystem.type.Name}");
+                HandleSubsystemPrint(subSystem, ref builder, depth + 1);
+            }
+        }
+    }
+}
