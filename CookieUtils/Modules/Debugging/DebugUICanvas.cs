482464aa93393b30e5659397f293fb2aaba17db2
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-10-05 15:53:29 +1000

    Small refactor for DebugUICanvas.cs

--- CookieUtils/Modules/Debugging/DebugUICanvas.cs
+++ CookieUtils/Modules/Debugging/DebugUICanvas.cs
@@ -16,6 +16,7 @@ namespace CookieUtils.Debugging
         private float _timeSinceLastCheck;
         private float _mouseCheckTime;
         private float _hideTime;
+        private State _state = State.Hidden;
 
         private void Awake()
         {
@@ -28,65 +29,86 @@ namespace CookieUtils.Debugging
 
         private void OnLockOn()
         {
-            if (_isHovering)
-                _isLockedOn = !_isLockedOn;
+            if (_isHovering) {
+                _state = _state switch {
+                    State.Hovered => State.Locked,
+                    State.Locked => State.Hovered,
+                    _ => _state
+                };
+            }
         }
-        
+
         public DebugUIPanel GetPanel(GameObject host)
         {
             if (_panel) return _panel;
-            
+
             _panel = host.GetComponentInChildren<DebugUIPanel>();
             if (_panel) return _panel;
 
             _renderer = host.GetComponentInChildren<Renderer>();
-            
+
             _canvas = gameObject.AddComponent<Canvas>();
             _canvas.renderMode = RenderMode.WorldSpace;
             _canvas.worldCamera = Camera.main;
-            
+
             if (!_panelPrefab) _panelPrefab = Resources.Load<DebugUIPanel>("DebugUI/Prefabs/Panel");
-            
+
             _panel = Instantiate(_panelPrefab, transform);
             _panel.gameObject.SetActive(false);
-            
+
             return _panel;
         }
 
         private void Update()
         {
             if (!CookieDebug.IsDebugMode) return;
-            
+
             _timeSinceLastCheck += Time.unscaledDeltaTime;
             _timeSinceHovered += Time.unscaledDeltaTime;
             if (_timeSinceLastCheck < _mouseCheckTime) return;
 
             _timeSinceLastCheck = 0;
-            
-            var mousePos = Mouse.current.position.ReadValue();
-            
-            bool doesIntersect = Bounds.IntersectRay(
-                _canvas.worldCamera.ScreenPointToRay(mousePos)
-            );
 
-            if (!doesIntersect && _canvas.enabled) {
-                doesIntersect = _panel.Image.rectTransform.rect.Contains(
-                    _panel.Image.rectTransform.InverseTransformPoint(_canvas.worldCamera.ScreenToWorldPoint(mousePos)));
-            }
+            _isHovering = CheckIntersection();
 
-            _isHovering = doesIntersect;
+            _state = _state switch {
+                State.Locked => State.Locked,
+                _ when _timeSinceHovered <= _hideTime => State.Hovered,
+                _ => State.Hidden
+            };
 
-            bool isEnabled = (_isLockedOn || _timeSinceHovered <= _hideTime) && CookieDebug.IsDebugMode;
+            bool isEnabled = _state is State.Locked or State.Hovered;
 
-            if (isEnabled) {
-                var color = _isLockedOn ? new Color(0.15f, 0.15f, 0.15f, 0.8f) : new Color(0.1f, 0.1f, 0.1f, 0.8f);
-                _panel.Image.color = color;
-            }
+            var color = _state switch {
+                State.Locked => new Color(0.15f, 0.15f, 0.15f, 0.8f),
+                _ => new Color(0.1f, 0.1f, 0.1f, 0.8f)
+            };
+            _panel.Image.color = color;
 
             _canvas.enabled = isEnabled;
             _panel.gameObject.SetActive(isEnabled);
 
             if (_isHovering) _timeSinceHovered = 0f;
+        }
+
+        private bool CheckIntersection()
+        {
+            var mousePos = Mouse.current.position.ReadValue();
+
+            if (_panel.gameObject.activeSelf && _panel.Image.rectTransform.rect.Contains(
+                    _panel.Image.rectTransform.InverseTransformPoint(
+                        _canvas.worldCamera.ScreenToWorldPoint(mousePos)
+                    )
+            )) return true;
+            
+            return Bounds.IntersectRay(_canvas.worldCamera.ScreenPointToRay(mousePos));
+        }
+
+        private enum State
+        {
+            Locked,
+            Hovered,
+            Hidden
         }
     }
 }
