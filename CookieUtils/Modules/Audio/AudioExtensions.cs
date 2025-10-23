a2d6e10e595ec2aaab7ecf5da7f02f808824a169
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-10-23 17:19:40 +1000

    Fix AudioExtensions.cs to use `IEnumerable.Random()` instead of the old `IEnumerable.PickRandom()`

--- CookieUtils/Modules/Audio/AudioExtensions.cs
+++ CookieUtils/Modules/Audio/AudioExtensions.cs
@@ -1,13 +1,15 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 namespace CookieUtils.Audio
 {
+    [PublicAPI]
     public static class AudioExtensions
     {
         public static void Play(this AudioClip clip, Vector3 pos, float volume = 1f, float spatialBlend = 0) => 
             AudioManager.PlaySound(clip, volume, pos, spatialBlend);
 
         public static void Play(this AudioClip[] clips, Vector3 pos, float volume = 1f, float spatialBlend = 0) => 
-            AudioManager.PlaySound(clips.PickRandom(), volume, pos, spatialBlend);
+            AudioManager.PlaySound(clips.Random(), volume, pos, spatialBlend);
     }
 }
