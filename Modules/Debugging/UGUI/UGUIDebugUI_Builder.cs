ef34830caf126e09db26527aee8b3830f2f1912d
cookie1170 <171882521+cookie1170@users.noreply.github.com>
2025-11-02 13:51:13 +1000

    Remove the unnecessary `InstantiateX` methods from `UGUIDebugUI_Helper`

--- Modules/Debugging/UGUI/UGUIDebugUI_Builder.cs
+++ Modules/Debugging/UGUI/UGUIDebugUI_Builder.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace CookieUtils.Debugging
 {
@@ -17,35 +18,40 @@ namespace CookieUtils.Debugging
         }
 
         public IDebugUIBuilder Label(Func<string> updateText) {
-            DebugUI_Label label = UGUIDebugUI_Helper.InstantiateLabel(updateText);
+            var label = Object.Instantiate<DebugUI_Label>(UGUIDebugUI_Helper.LabelPrefab);
+            label.Init(updateText);
             Add(label);
 
             return this;
         }
 
         public IDebugUIBuilder FloatField(string text, Func<float> updateValue, Action<float> onValueEdited) {
-            DebugUI_FloatField field = UGUIDebugUI_Helper.InstantiateFloatField(text, updateValue, onValueEdited);
+            var field = Object.Instantiate<DebugUI_FloatField>(UGUIDebugUI_Helper.FloatFieldPrefab);
+            field.Init(text, updateValue, onValueEdited);
             Add(field);
 
             return this;
         }
 
         public IDebugUIBuilder IntField(string text, Func<int> updateValue, Action<int> onValueEdited) {
-            DebugUI_IntField field = UGUIDebugUI_Helper.InstantiateIntField(text, updateValue, onValueEdited);
+            var field = Object.Instantiate<DebugUI_IntField>(UGUIDebugUI_Helper.IntFieldPrefab);
+            field.Init(text, updateValue, onValueEdited);
             Add(field);
 
             return this;
         }
 
         public IDebugUIBuilder BoolField(string text, Func<bool> updateValue, Action<bool> onValueEdited) {
-            DebugUI_BoolField field = UGUIDebugUI_Helper.InstantiateBoolField(text, updateValue, onValueEdited);
+            var field = Object.Instantiate<DebugUI_BoolField>(UGUIDebugUI_Helper.BoolFieldPrefab);
+            field.Init(text, updateValue, onValueEdited);
             Add(field);
 
             return this;
         }
 
         public IDebugUIBuilder StringField(string text, Func<string> updateValue, Action<string> onValueEdited) {
-            DebugUI_StringField field = UGUIDebugUI_Helper.InstantiateStringField(text, updateValue, onValueEdited);
+            var field = Object.Instantiate<DebugUI_StringField>(UGUIDebugUI_Helper.StringFieldPrefab);
+            field.Init(text, updateValue, onValueEdited);
             Add(field);
 
             return this;
@@ -58,9 +64,8 @@ namespace CookieUtils.Debugging
             string xLabel = "x",
             string yLabel = "y"
         ) {
-            DebugUI_Vector2Field field = UGUIDebugUI_Helper.InstantiateVector2Field(
-                text, updateValue, onValueEdited, xLabel, yLabel
-            );
+            var field = Object.Instantiate<DebugUI_Vector2Field>(UGUIDebugUI_Helper.Vector2FieldPrefab);
+            field.Init(text, updateValue, onValueEdited, xLabel, yLabel);
             Add(field);
 
             return this;
@@ -74,23 +79,24 @@ namespace CookieUtils.Debugging
             string yLabel = "y",
             string zLabel = "z"
         ) {
-            DebugUI_Vector3Field field = UGUIDebugUI_Helper.InstantiateVector3Field(
-                text, updateValue, onValueEdited, xLabel, yLabel, zLabel
-            );
+            var field = Object.Instantiate<DebugUI_Vector3Field>(UGUIDebugUI_Helper.Vector3FieldPrefab);
+            field.Init(text, updateValue, onValueEdited, xLabel, yLabel, zLabel);
             Add(field);
 
             return this;
         }
 
         public IDebugUIBuilder Button(Func<string> updateText, Action onClicked) {
-            DebugUI_Button button = UGUIDebugUI_Helper.InstantiateButton(updateText, onClicked);
+            var button = Object.Instantiate<DebugUI_Button>(UGUIDebugUI_Helper.ButtonPrefab);
+            button.Init(updateText, onClicked);
             Add(button);
 
             return this;
         }
 
         public IDebugUIBuilder FoldoutGroup(Func<string> updateText, bool defaultShown) {
-            DebugUI_FoldoutGroup foldout = UGUIDebugUI_Helper.InstantiateFoldout(updateText, defaultShown);
+            var foldout = Object.Instantiate<DebugUI_FoldoutGroup>(UGUIDebugUI_Helper.FoldoutGroupPrefab);
+            foldout.Init(updateText, defaultShown);
             Add(foldout);
             _groups.Push(foldout);
 
@@ -98,7 +104,8 @@ namespace CookieUtils.Debugging
         }
 
         public IDebugUIBuilder IfGroup(Func<bool> condition) {
-            DebugUI_IfGroup group = UGUIDebugUI_Helper.InstantiateIfGroup(condition);
+            var group = Object.Instantiate<GameObject>(UGUIDebugUI_Helper.GroupPrefab).AddComponent<DebugUI_IfGroup>();
+            group.Init(condition);
             Add(group);
             _groups.Push(group);
 
@@ -113,7 +120,9 @@ namespace CookieUtils.Debugging
                     "[CookieUtils.Debugging] An else group can only be started after an if group!"
                 );
 
-            DebugUI_ElseGroup group = UGUIDebugUI_Helper.InstantiateElseGroup(ifGroup);
+            var group = Object.Instantiate<GameObject>(UGUIDebugUI_Helper.GroupPrefab)
+                .AddComponent<DebugUI_ElseGroup>();
+            group.Init(ifGroup);
             Add(group);
             _groups.Push(group);
 
@@ -121,14 +130,18 @@ namespace CookieUtils.Debugging
         }
 
         public IDebugUIBuilder SwitchGroup<T>(Func<T> condition) {
-            DebugUI_SwitchGroup<T> group = UGUIDebugUI_Helper.InstantiateSwitchGroup(condition);
+            var group = Object.Instantiate<GameObject>(UGUIDebugUI_Helper.GroupPrefab)
+                .AddComponent<DebugUI_SwitchGroup<T>>();
+            group.Init(condition);
             Add(group);
 
             return this;
         }
 
         public IDebugUIBuilder CaseGroup<T>(Func<T> value) {
-            DebugUI_CaseGroup<T> group = UGUIDebugUI_Helper.InstantiateCaseGroup(value);
+            var group = Object.Instantiate<GameObject>(UGUIDebugUI_Helper.GroupPrefab)
+                .AddComponent<DebugUI_CaseGroup<T>>();
+            group.Init(value);
             Add(group);
 
             return this;
--- Modules/Debugging/UGUI/UGUIDebugUI_Helper.cs
+++ Modules/Debugging/UGUI/UGUIDebugUI_Helper.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -7,142 +6,19 @@ namespace CookieUtils.Debugging
     // ReSharper disable once InconsistentNaming
     internal static class UGUIDebugUI_Helper
     {
-        private static readonly Prefab<DebugUI_FoldoutGroup> FoldoutGroupPrefab = "FoldoutGroup";
-        private static readonly Prefab<DebugUI_Label> LabelPrefab = "Label";
-        private static readonly Prefab<GameObject> GroupPrefab = "Group";
-        private static readonly Prefab<DebugUI_Button> ButtonPrefab = "Button";
-        private static readonly Prefab<DebugUI_FloatField> FloatFieldPrefab = "Fields/FloatField";
-        private static readonly Prefab<DebugUI_IntField> IntFieldPrefab = "Fields/IntField";
-        private static readonly Prefab<DebugUI_BoolField> BoolFieldPrefab = "Fields/BoolField";
-        private static readonly Prefab<DebugUI_StringField> StringFieldPrefab = "Fields/StringField";
-        private static readonly Prefab<DebugUI_Vector2Field> Vector2FieldPrefab = "Fields/Vector2Field";
-        private static readonly Prefab<DebugUI_Vector3Field> Vector3FieldPrefab = "Fields/Vector3Field";
-        private static readonly Prefab<DebugUI_Panel> PanelPrefab = "Panel";
+        internal static readonly Prefab<DebugUI_FoldoutGroup> FoldoutGroupPrefab = "FoldoutGroup";
+        internal static readonly Prefab<DebugUI_Label> LabelPrefab = "Label";
+        internal static readonly Prefab<GameObject> GroupPrefab = "Group";
+        internal static readonly Prefab<DebugUI_Button> ButtonPrefab = "Button";
+        internal static readonly Prefab<DebugUI_FloatField> FloatFieldPrefab = "Fields/FloatField";
+        internal static readonly Prefab<DebugUI_IntField> IntFieldPrefab = "Fields/IntField";
+        internal static readonly Prefab<DebugUI_BoolField> BoolFieldPrefab = "Fields/BoolField";
+        internal static readonly Prefab<DebugUI_StringField> StringFieldPrefab = "Fields/StringField";
+        internal static readonly Prefab<DebugUI_Vector2Field> Vector2FieldPrefab = "Fields/Vector2Field";
+        internal static readonly Prefab<DebugUI_Vector3Field> Vector3FieldPrefab = "Fields/Vector3Field";
+        internal static readonly Prefab<DebugUI_Panel> PanelPrefab = "Panel";
 
-        internal static DebugUI_Label InstantiateLabel(Func<string> updateText) {
-            var label = Object.Instantiate<DebugUI_Label>(LabelPrefab);
-            label.Init(updateText);
-
-            return label;
-        }
-
-        internal static DebugUI_FloatField InstantiateFloatField(
-            string text,
-            Func<float> updateValue,
-            Action<float> onValueEdited
-        ) {
-            var field = Object.Instantiate<DebugUI_FloatField>(FloatFieldPrefab);
-            field.Init(text, updateValue, onValueEdited);
-
-            return field;
-        }
-
-        internal static DebugUI_IntField InstantiateIntField(
-            string text,
-            Func<int> updateValue,
-            Action<int> onValueEdited
-        ) {
-            var field = Object.Instantiate<DebugUI_IntField>(IntFieldPrefab);
-            field.Init(text, updateValue, onValueEdited);
-
-            return field;
-        }
-
-        internal static DebugUI_BoolField InstantiateBoolField(
-            string text,
-            Func<bool> updateValue,
-            Action<bool> onValueEdited
-        ) {
-            var field = Object.Instantiate<DebugUI_BoolField>(BoolFieldPrefab);
-            field.Init(text, updateValue, onValueEdited);
-
-            return field;
-        }
-
-        internal static DebugUI_StringField InstantiateStringField(
-            string text,
-            Func<string> updateValue,
-            Action<string> onValueEdited
-        ) {
-            var field = Object.Instantiate<DebugUI_StringField>(StringFieldPrefab);
-            field.Init(text, updateValue, onValueEdited);
-
-            return field;
-        }
-
-        internal static DebugUI_Vector2Field InstantiateVector2Field(
-            string text,
-            Func<Vector2> updateValue,
-            Action<Vector2> onValueEdited,
-            string xLabel,
-            string yLabel
-        ) {
-            var field = Object.Instantiate<DebugUI_Vector2Field>(Vector2FieldPrefab);
-            field.Init(text, updateValue, onValueEdited, xLabel, yLabel);
-
-            return field;
-        }
-
-        internal static DebugUI_Vector3Field InstantiateVector3Field(
-            string text,
-            Func<Vector3> updateValue,
-            Action<Vector3> onValueEdited,
-            string xLabel,
-            string yLabel,
-            string zLabel
-        ) {
-            var field = Object.Instantiate<DebugUI_Vector3Field>(Vector3FieldPrefab);
-            field.Init(text, updateValue, onValueEdited, xLabel, yLabel, zLabel);
-
-            return field;
-        }
-
-        internal static DebugUI_Button InstantiateButton(Func<string> updateText, Action onClicked) {
-            var button = Object.Instantiate<DebugUI_Button>(ButtonPrefab);
-            button.Init(updateText, onClicked);
-
-            return button;
-        }
-
-        internal static DebugUI_FoldoutGroup InstantiateFoldout(Func<string> updateText, bool defaultShown) {
-            var foldout = Object.Instantiate<DebugUI_FoldoutGroup>(FoldoutGroupPrefab);
-            foldout.Init(updateText, defaultShown);
-
-            return foldout;
-        }
-
-        internal static DebugUI_IfGroup InstantiateIfGroup(Func<bool> condition) {
-            var group = Object.Instantiate<GameObject>(GroupPrefab).AddComponent<DebugUI_IfGroup>();
-            group.Init(condition);
-
-            return group;
-        }
-
-        internal static DebugUI_ElseGroup InstantiateElseGroup(DebugUI_IfGroup ifGroup) {
-            var group = Object.Instantiate<GameObject>(GroupPrefab).AddComponent<DebugUI_ElseGroup>();
-            group.Init(ifGroup);
-
-            return group;
-        }
-
-        internal static DebugUI_SwitchGroup<T> InstantiateSwitchGroup<T>(Func<T> condition) {
-            var group = Object.Instantiate<GameObject>(GroupPrefab).AddComponent<DebugUI_SwitchGroup<T>>();
-            group.Init(condition);
-
-            return group;
-        }
-
-        internal static DebugUI_CaseGroup<T> InstantiateCaseGroup<T>(Func<T> value) {
-            var group = Object.Instantiate<GameObject>(GroupPrefab).AddComponent<DebugUI_CaseGroup<T>>();
-            group.Init(value);
-
-            return group;
-        }
-
-        internal static DebugUI_Panel InstantiatePanel(Transform parent) =>
-            Object.Instantiate<DebugUI_Panel>(PanelPrefab, parent);
-
-        private class Prefab<T> where T : Object
+        internal class Prefab<T> where T : Object
         {
             private readonly string _path;
             private T _prefabCached;
